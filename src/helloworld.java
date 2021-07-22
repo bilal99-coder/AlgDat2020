543bff19e0829abacaabc4de2a9579277f15a6ef
Bilal Sahli <Bilalsahli1999@gmail.com>
2021-07-22 21:30:33 +0200

    Youssef teller til tyve

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,5 +1,9 @@
 public class helloworld {
     public static void main (String [] args){
         System.out.println("Hello world");
+        System.out.println("Youssef teller til tyve");
+        for(int i =0 ; i<21 ; i++){
+            System.out.println(i);
+        }
     }
 }
